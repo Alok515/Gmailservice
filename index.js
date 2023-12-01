25e1fac66e3e52525e858657850e4c7f9200f14d
Alok <kumarsinghalok28@gmail.com>
2023-12-01 13:37:06 +0530

    added auth to route

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const functions = require('@google-cloud/functions-framework');
-
+const { auth } =  require('express-oauth2-jwt-bearer');
 const express = require('express');
 const cors = require('cors');
 const db = require('./config/MongooseConnect');
 db();
+
+const {
+    Audience,
+    IssuerUrl,
+    Algorithm
+} = process.env;
+
+
 const app = express();
 app.use(cors());
+
+const jwtCheck = auth({
+    audience: Audience,
+    issuerBaseURL: IssuerUrl,
+    tokenSigningAlg: Algorithm
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -13,6 +28,7 @@ app.use(express.urlencoded({extended: true}));
 const route = require('./router/index');
 const errorHandle = require('./middleware/errorHandle');
 
+app.use(jwtCheck);
 app.use(route);
 app.use(errorHandle);
 
